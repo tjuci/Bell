bcc6e5c3d42f238d6ea6cd485d1886754806b4ce
andrew <tjuci@sina.com>
2017-05-25 15:02:57 +0800

    Initial Commit

---
+++ Bell/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Bell
+//
+//  Created by Andrew-mac on 2017/5/25.
+//  Copyright © 2017年 SITRI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BellTests/BellTests.m
@@ -0,0 +1,39 @@
+//
+//  BellTests.m
+//  BellTests
+//
+//  Created by Andrew-mac on 2017/5/25.
+//  Copyright © 2017年 SITRI. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BellTests : XCTestCase
+
+@end
+
+@implementation BellTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
